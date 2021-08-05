8f337b75960456574ad6d7dda1ea3471e9824ab5
Alexandre Menezes <amenezes031@gmail.com>
2021-08-05 16:36:14 -0300

    add styles in the input restaurants search and carousel

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import Home from './pages/Home';
 import theme from './styles/theme';
 import GlobalStyle from './styles/global';
 
+// CSS - Input restaurants search
+import '@material/react-text-field/dist/text-field.css';
+import '@material/react-material-icon/dist/material-icon.css';
+
+// CSS - Carousel
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
