b21e84c7701fb2a068aa942d0d27e65a29eb7ddb
Alexandre Menezes <amenezes031@gmail.com>
2021-08-05 16:37:23 -0300

    add logo, input search and carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
+import Slider from 'react-slick';
 
-import { Container } from './styles';
+import LogoImage from '../../assets/logo.svg';
+import RestaurantImage from '../../assets/restaurant-fake.png';
+
+import { Wrapper, Container, Search, Logo, Map, Carousel, CarouselTitle } from './styles';
 
 export default function Home() {
-  return <Container>Hello World!</Container>;
+  const [inputValue, setInputValue] = useState('');
+
+  function handleInputValue(event) {
+    const { value } = event.currentTarget;
+    setInputValue(value);
+  }
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={LogoImage} alt="Logo Restaurante Finder" />
+          <TextField
+            label="Pesquisar Restaurantes"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(event) => handleInputValue(event)} />
+          </TextField>
+        </Search>
+
+        <Carousel>
+          <CarouselTitle>Na sua Área</CarouselTitle>
+
+          <Slider {...settings}>
+            <div>
+              <img
+                src={RestaurantImage}
+                alt="Restautante
+              fake"
+              />
+            </div>
+            <div>
+              <img
+                src={RestaurantImage}
+                alt="Restautante
+              fake"
+              />
+            </div>
+            <div>
+              <img
+                src={RestaurantImage}
+                alt="Restautante
+              fake"
+              />
+            </div>
+            <div>
+              <img
+                src={RestaurantImage}
+                alt="Restautante
+              fake"
+              />
+            </div>
+            <div>
+              <img
+                src={RestaurantImage}
+                alt="Restautante
+              fake"
+              />
+            </div>
+          </Slider>
+        </Carousel>
+      </Container>
+
+      <Map>
+        <h1>Mapa</h1>
+      </Map>
+    </Wrapper>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,53 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
 export const Container = styled.aside`
-  background-color: ${({ theme }) => theme.colors.background};
-  width: 22.5rem;
+  width: 360px;
   height: 100vh;
+  background-color: ${({ theme }) => theme.colors.background};
   overflow-y: auto;
+`;
+
+export const Search = styled.section`
+  background-color: #fff;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Logo = styled.img`
+  margin: 0 auto;
+  height: 5.625rem;
+  width: 9.375rem;
+`;
+
+export const Carousel = styled.div`
+  background-color: #fff;
+  padding: 0 1rem;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CarouselTitle = styled.h2`
+  color: ${({ theme }) => theme.colors.text};
+  font-size: 1.5rem;
+  font-weight: bold;
+  line-height: 2rem;
+`;
+
+export const Map = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: red;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
 `;
