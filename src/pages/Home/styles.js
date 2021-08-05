22b8c8d48c6292bcd48cf40299b68df7c58b3277
Alexandre Menezes <amenezes031@gmail.com>
2021-08-05 12:37:01 -0300

    create home container style

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  background-color: ${({ theme }) => theme.colors.background};
+  width: 22.5rem;
+  height: 100vh;
+  overflow-y: auto;
+`;
