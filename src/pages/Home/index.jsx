fa33aa3f52b5863bfea98c042727bc505e53b802
Alexandre Menezes <amenezes031@gmail.com>
2021-08-05 12:37:29 -0300

    add container style in the home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Container } from './styles';
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return <Container>Hello World!</Container>;
 }
