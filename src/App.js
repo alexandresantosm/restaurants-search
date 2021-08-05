366d102190c84d21bb443af5f8f52ae7c57b5399
Alexandre Menezes <amenezes031@gmail.com>
2021-08-05 11:26:35 -0300

    add theme, global styles and home page in the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import Home from './pages/Home';
+
+import theme from './styles/theme';
+import GlobalStyle from './styles/global';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
 
 export default App;
