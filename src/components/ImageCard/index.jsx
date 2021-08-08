8df1148fa079ce5f0bd5eebd5aa933d93cf598e8
Alexandre Menezes <amenezes031@gmail.com>
2021-08-08 16:00:35 -0300

    add image skeleton from image card component

--- src/components/ImageCard/index.jsx
+++ src/components/ImageCard/index.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import Skeleton from '../Skeleton';
 import { Container, Title } from './styles';
 
 export default function ImageCard({ photo, title }) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const imageLoader = new Image();
+
+    imageLoader.src = photo;
+
+    imageLoader.onload = () => setImageLoaded(true);
+  }, [photo]);
+
   return (
-    <Container photo={photo}>
-      <Title>{title}</Title>
-    </Container>
+    <>
+      {imageLoaded ? (
+        <Container photo={photo}>
+          <Title>{title}</Title>
+        </Container>
+      ) : (
+        <Skeleton width="5.625rem" height="5.625rem" />
+      )}
+    </>
   );
 }
